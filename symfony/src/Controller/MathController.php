b25469bee6d54719cc9209514890f5a193caec2d
pablomuro <pablo.muro@gmail.com>
2023-05-04 12:06:08 +0200

    Math endpoint

--- symfony/src/Controller/MathController.php
+++ symfony/src/Controller/MathController.php
@@ -9,21 +9,34 @@ use Symfony\Component\HttpFoundation\Request;
 
 class MathController extends AbstractController
 {
-    #[Route('/math', name: 'app_math')]
-    public function index(Request $request): JsonResponse
-    {
-        $numberList = $request->query->get('numbers') ?? null;
-        $number = $request->query->get('number') ?? null;
+  #[Route('/math', name: 'app_math')]
+  public function index(Request $request): JsonResponse
+  {
+    $number = $request->query->get('number') ?? null;
 
-        if($number){
-            return $this->json((int) $number + 1);
-        }
-
-        if($numberList){
-            var_dump(json_decode($numberList));
-            return $this->json('numberList test');
-        }
-
-        return $this->json('Nothing to do');
+    if($number != null){
+      if(is_numeric($number)) return $this->json((int) $number + 1);
+      if(!is_numeric($number)) return $this->json('Invalid query parameter number: Not a number');
     }
+
+    $numberList = $request->query->get('numbers') ?? null;
+
+    if($numberList != null){
+      if(json_decode($numberList) != null){
+        $numberList = json_decode($numberList);
+
+        $result = $numberList[0];
+        for ($i = 1; $i < sizeof($numberList); $i++){
+          $result = ((($numberList[$i] * $result)) /
+                  (gmp_intval(gmp_gcd($numberList[$i], $result))));
+        }
+
+        return $this->json($result);
+      } else {
+        return $this->json('Invalid query parameter numbers: Not a list');
+      }
+    }
+
+    return $this->json('No query parameter passed');
+  }
 }
